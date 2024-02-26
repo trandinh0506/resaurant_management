68660e307d3b95fa7b567324fd6c740aba9b54ff
Dinh Tran <tvandinh93@gmail.com>
2024-02-26 07:36:48 +0700

    catching error, logging req.body

--- Controllers/admin.Controller.js
+++ Controllers/admin.Controller.js
@@ -1,10 +1,20 @@
 const Product = require("../Models/product.Model");
 class adminController {
     async addProduct(req, res) {
-        const { name, price, description, category } = req.body;
-        const newProduct = new Product({ name, price, description, category });
-        await newProduct.save();
-        res.json({ status: 200, message: "Product added successfully!" });
+        try {
+            console.log(req.body);
+            const { name, price, description, category } = req.body;
+            const newProduct = new Product({
+                name,
+                price,
+                description,
+                category,
+            });
+            await newProduct.save();
+            res.json({ status: 200, message: "Product added successfully!" });
+        } catch (err) {
+            res.status(500).json({ status: 500, message: err.message });
+        }
     }
     async removeProduct(req, res) {}
     async updateProduct(req, res) {}
